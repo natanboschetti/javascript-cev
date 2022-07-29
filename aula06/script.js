695163be3de027b69c3abce5e23a4813bd7aa0b8
Natan Boschetti <natanszbochetti@gmail.com>
2022-07-29 16:29:37 -0300

    Tratamento e manipulação de dados + operadores

--- aula06/script.js
+++ aula06/script.js
@@ -1,2 +1,14 @@
-let name = window.prompt(`Qual é o seu nome?`)
-window.alert(`O nome é ${name}`)
+//let number1 = 1545.5
+//console.log(number1.toFixed(2).replace('.', ','))
+//console.log(number1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}).replace('.',','))
+
+//console.log(5 === '5')
+
+//let media = 5.5
+//console.log(media > 7?'Aprovado':'Reprovado') // operador ternário
+
+//let a = 1
+//let b = 2
+//console.log(a > b?'Sim':'Não')
+//console.log(a < b && 2 == 2)
+//console.log(a < b || 2 == 0)
