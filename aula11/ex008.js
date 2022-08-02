7825d33c064c371ef8c06280b55d390062c26028
Natan Boschetti <natanszbochetti@gmail.com>
2022-08-02 18:47:09 -0300

    conditions ex

---
+++ aula11/ex008.js
@@ -0,0 +1,15 @@
+/*
+let speed = 78
+console.log(`A velocidade do seu carro é ${speed}km/h`)
+if (speed > 80) {
+    console.log(`Está acima do limite`)
+}
+*/
+
+let country = 'brasil'
+if (country == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}   
+
