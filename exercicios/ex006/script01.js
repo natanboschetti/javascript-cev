0e7cc39a76b442ba2f1eaeb19f769dc8f0ed916a
Natan Boschetti <natansb0787@gmail.com>
2022-08-18 14:45:14 -0300

    ex006 finished

--- exercicios/ex006/script01.js
+++ exercicios/ex006/script01.js
@@ -3,7 +3,7 @@ let selector = document.querySelector(`select#selector`)
 let result = document.querySelector(`div#result`)
 let list = []
 
-function isNumber(n) {
+function isNumber(n) { // verifica se é número
     if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
@@ -11,7 +11,7 @@ function isNumber(n) {
     }
 }
 
-function inList(n, l) {
+function inList(n, l) { // verifica se a lista ja contém o valor
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -19,7 +19,7 @@ function inList(n, l) {
     }
 }
 
-function addNumber() {
+function addNumber() { //adiciona número de acordo com as condições que são configuradas através de functions
     if (isNumber(number.value) && !inList(number.value, list)) {
         list.push(Number(number.value))
         let item = document.createElement('option')
@@ -33,31 +33,40 @@ function addNumber() {
     number.focus()
 }
 
-function finish() {
+function cleanNumber() { //limpar toda a página, result + select
+    list = []
+    result.innerHTML = ''
+    selector.innerText = ''
+    number.value = ''
+    number.focus()
+}
+
+function finish() { //mostra todos os resultados
     if (list.length == 0) {
         alert(`Erro! Adicione um valor.`)
     } else {
         let total = list.length
         let larger = list[0]
         let smaller = list[0]
-        for (let position in list) {
+        for (let position in list) { //qual o menor e maior valor
             if (list[position] > larger) {
                 larger = list[position]
             } if (list[position] < smaller) {
                 smaller = list[position]
             }
         }
-
         sum = 0
-        for (let i = 0; i < list.length; i++) {
+        for (let i = 0; i < list.length; i++) { // soma
             sum += list[i] //a variável i vai estar sempre mudando, assim sendo posições do array.
             
         }
-
+        average = sum / total
         result.innerHTML = ''
         result.innerHTML += `<p>Ao todo temos ${total} números adicionados.</p>`
         result.innerHTML += `<p>O maior valor adicionado foi ${larger}.</p>`
         result.innerHTML += `<p>O menor valor adicionado foi ${smaller}.</p>`
         result.innerHTML += `<p>Somando todos os valores, temos ${sum}.</p>`
+        result.innerHTML += `<p>A média dos valores digitados é ${average}.</p>`
     }
 }
+list = []
